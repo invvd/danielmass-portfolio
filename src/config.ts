4cf243062628de634005bace3c30226301954223
Danny <danymassmcf@gmail.com>
2025-10-21 00:49:35 -0300

    refactor(config): extract config to config.ts

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const navBarLinks = [
+  { href: "#hero", label: "Inicio" },
+  { href: "#services", label: "Servicios" },
+  { href: "#projects", label: "Proyectos" },
+  { href: "#about", label: "Sobre mí" },
+];
