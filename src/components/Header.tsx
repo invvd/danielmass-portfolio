01729bd24032e9e4b781ea3ab017a8bcba4f4925
Danny <danymassmcf@gmail.com>
2025-10-22 18:06:27 -0300

    style: change cta button style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,17 @@ import MenuIcon from "../icons/MenuIcon";
 import { useMenuContext } from "../contexts/useMenuContext";
 import ChatBubbleIcon from "../icons/ChatBubbleIcon";
 import { navBarLinks } from "../config";
-import { motion } from "motion/react";
 
 export default function Header() {
   const { toggleMenu } = useMenuContext();
 
   return (
-    <header className="border border-white/2 backdrop-brightness-50 p-3 px-6 top-4 rounded-full backdrop-blur-lg inset-0 h-fit mx-auto fixed w-[90dvw] flex justify-between items-center z-50">
+    <header className="border border-white/2 backdrop-brightness-50 p-3 px-6 top-4 rounded-full backdrop-blur-lg left-1/2 -translate-x-1/2 h-fit mx-auto fixed w-[90dvw] flex justify-between items-center z-50">
       {/* Logo */}
-      <a href="/" className="text-white ">
+      <a
+        href="/"
+        className="text-white transition-all hover:text-shadow-md text-shadow-primary/40 duration-200"
+      >
         Daniel Mass
       </a>
 
@@ -33,7 +35,7 @@ export default function Header() {
       {/* Mobile Menu Button */}
       <button
         onClick={toggleMenu}
-        className="text-white/80 hover:text-white cursor-pointer transition-colors md:hidden"
+        className="text-white/80 hover:text-white cursor-pointer md:hidden transition-all hover:text-shadow-md text-shadow-primary/40 duration-200"
       >
         <MenuIcon className="size-6" />
       </button>
@@ -41,7 +43,7 @@ export default function Header() {
       {/* CTA */}
       <a
         href="#contact"
-        className="bg-primary/10 text-white/80 px-4 py-1 rounded-full hover:bg-primary/20 transition-colors font-medium items-center hidden md:flex gap-2"
+        className="bg-gradient-to-r from-primary/10 to-primary/20 border border-white/5 text-white/80 px-4 py-1 rounded-full hover:to-primary/30 transition-colors font-medium items-center hidden md:flex gap-2"
       >
         <span>Contáctame!</span>
         <span>
