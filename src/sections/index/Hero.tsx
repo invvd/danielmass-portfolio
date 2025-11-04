2d3b1238442e504028a66f0f12c16f520b45bce5
Danny <danymassmcf@gmail.com>
2025-11-04 17:56:27 -0300

    fix: set animations to play once and add aria-label to the down arrow button

--- src/sections/index/Hero.tsx
+++ src/sections/index/Hero.tsx
@@ -6,12 +6,13 @@ export default function Hero() {
   return (
     <motion.section
       id="hero"
-      className="w-dvw relative h-lvh flex flex-col items-center justify-center p-10 bg-gradient-to-b from-black/60 via-black/60 via-60% to-transparent text-center gap-4"
+      className="w-dvw relative h-lvh flex flex-col items-center justify-center p-10 text-center gap-2"
     >
       <motion.div
         className="flex flex-col-reverse gap-4 md:items-center"
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         transition={{ duration: 0.6, ease: "easeOut" }}
       >
         <h1>Soy Daniel Mass, desarrollador web full stack 💻.</h1>
@@ -21,6 +22,7 @@ export default function Hero() {
         className="text-balance max-w-2xl text-white/80"
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
       >
         Transformo tus ideas en soluciones digitales funcionales, rápidas y
@@ -29,6 +31,7 @@ export default function Hero() {
       <motion.div
         initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         transition={{ duration: 0.6, delay: 0.4, ease: "easeOut" }}
         className="flex flex-col-reverse md:flex-row gap-4 mt-6 items-center justify-center"
       >
@@ -41,8 +44,9 @@ export default function Hero() {
         </a>
       </motion.div>
       <a
+        aria-label="Ir a Servicios"
         href="/#services"
-        className="absolute bottom-20 hover:bg-primary/10 p-2 rounded-full transition animate-pulse"
+        className="absolute bottom-16 hover:bg-primary/10 p-2 rounded-full transition animate-pulse"
       >
         <ArrowDownIcon className="size-8 text-white/80" />
       </a>
@@ -56,11 +60,7 @@ function StatusTag({ status }: { status: "Disponible" | "No disponible" }) {
       className={`bg-gradient-to-tr from-black to-white/10 w-fit rounded-full text-sm py-1 px-3 flex items-center mx-auto gap-2 shadow shadow-primary/10`}
     >
       <span>{status}</span>
-      <div
-        className={`bg-${
-          status === "Disponible" ? "green" : "red"
-        }-500 size-1.5 rounded-full`}
-      />
+      <div className={`bg-green-500 size-1.5 rounded-full`} />
     </div>
   );
 }
