db3a7351d8575f950715576bbbc3ded5cb21ed10
Danny <danymassmcf@gmail.com>
2025-11-11 00:01:39 -0300

    refactor: create component for reuse

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,35 @@
+export default function PrimaryButton({
+  href,
+  cta,
+  Icon,
+  className,
+  disabled,
+}: {
+  href: string;
+  cta: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  className?: string;
+  disabled?: boolean;
+}) {
+  if (disabled) {
+    return (
+      <div
+        className={`bg-gradient-to-r from-primary/10 to-primary/30 border border-white/5 text-white/80 px-2 py-1 rounded-full shadow-lg transition-colors items-center flex gap-2 group ${className}`}
+      >
+        <div className="opacity-25 flex items-center gap-2 cursor-not-allowed">
+          {cta} <Icon className="size-5" />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <a
+      aria-disabled={disabled}
+      href={href}
+      className={`bg-gradient-to-r from-primary/10 to-primary/30 border border-white/5 text-white/80 px-2 py-1 rounded-full hover:to-primary/30 hover:text-white shadow-lg transition-colors items-center flex gap-2 group ${className}`}
+    >
+      {cta} <Icon className="size-5" />
+    </a>
+  );
+}
