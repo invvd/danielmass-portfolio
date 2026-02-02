3740c61877480fa639c72de6d816b81f74c32ec4
Danny <danymassmcf@gmail.com>
2026-02-02 13:59:07 -0300

    add purpose selector

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -7,35 +7,37 @@ export default function PrimaryButton({
   target,
   rel,
 }: {
-  href: string;
+  href?: string;
   cta: string;
-  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   className?: string;
   disabled?: boolean;
   target?: string;
   rel?: string;
 }) {
-  if (disabled) {
+  if (href) {
     return (
-      <div
-        className={`bg-linear-to-r from-primary/10 to-primary/30 border border-white/5 text-white/80 px-2 py-1 rounded-full shadow-lg transition-all items-center flex gap-2 group ${className}`}
+      <a
+        rel={rel}
+        target={target}
+        aria-disabled={disabled}
+        {...(href ? { href } : {})}
+        className={`bg-linear-to-r from-primary/10 to-primary/30 border border-white/5 text-white/80 px-2 py-1 rounded-full hover:to-primary/30 hover:text-white shadow-lg transition-all items-center flex gap-2 group ${className}`}
       >
-        <div className="opacity-25 flex items-center gap-2 cursor-not-allowed">
-          {cta} <Icon className="size-5" />
-        </div>
-      </div>
+        {cta} {Icon && <Icon className="size-5" />}
+      </a>
     );
   }
 
   return (
-    <a
-      rel={rel}
-      target={target}
-      aria-disabled={disabled}
-      href={href}
-      className={`bg-linear-to-r from-primary/10 to-primary/30 border border-white/5 text-white/80 px-2 py-1 rounded-full hover:to-primary/30 hover:text-white shadow-lg transition-all items-center flex gap-2 group ${className}`}
+    <div
+      className={`bg-linear-to-r from-primary/10 to-primary/30 border border-white/5 text-white/80 px-2 py-1 rounded-full shadow-lg transition-all items-center flex gap-2 group ${className}`}
     >
-      {cta} <Icon className="size-5" />
-    </a>
+      <div
+        className={`flex items-center gap-2 ${disabled ? "cursor-not-allowed opacity-25" : ""}`}
+      >
+        {cta} {Icon && <Icon className="size-5" />}
+      </div>
+    </div>
   );
 }
